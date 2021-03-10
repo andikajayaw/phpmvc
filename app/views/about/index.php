56875c4e7134f7d4ece2903193afa5e8128036fc
andikajayaw <andikajayawiguna@gmail.com>
2021-03-10 22:32:23 +0800

    update assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,7 @@
-
-<h1>About Index Page</h1>
-<p>Halo, nama saya <?= $data['nama']; ?>, umur saya  <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>  </p>
+<div class="container">
+    <div class="mt-4">
+        <img src="<?= BASEURL; ?>img/profile.png" alt="profile" width="200" class="rounded-circle shadow">
+        <h1>About Index Page</h1>
+        <p>Halo, nama saya <?= $data['nama']; ?>, umur saya  <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>  </p>
+    </div>
+</div>
